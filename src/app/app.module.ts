cc807482fd57dc2abe3a2561ee81b019dca4c808
zych.johann@gmail.com <zych.johann@gmail.com>
2023-04-18 09:42:04 +0200

    Quete 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    MenuComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
